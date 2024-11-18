16066ed16ca2151835a83f48aa2d5960b751d534
RmsCarvalho <rmscarvalho@fc.ul.pt>
2024-11-18 23:59:23 +0000

    update buildGraph

--- mimicreadmission/resources/buildGraph.py
+++ mimicreadmission/resources/buildGraph.py
@@ -3,7 +3,7 @@ from operator import itemgetter
 import rdflib
 from rdflib.namespace import RDF, OWL, RDFS
 from rdflib import URIRef
-
+from tqdm import tqdm
 
 # The format of the input files is allwaays: (id);[annoations]
 # as such we can always split by ; and than loop the list
@@ -24,60 +24,60 @@ def construct_kg(ontologySet,annotationSet,annotationType):
         kg = rdflib.Graph()
         ents = set()
 
-        for loc in range(len(ontologySet)):
+        for loc in tqdm(range(len(ontologySet)),desc="Loading Ontologies", unit="ontology"):
 
-            extension = ontologySet[loc].split('.')[-1]
+            extension = ontologySet[loc].split(".")[-1]
             format = file_formats.get(f".{extension.lower()}", None)
 
-            if 'ICD9' not in ontologySet[loc]:
+            if "ICD9" not in ontologySet[loc]:
+                print('PARSING THE ONTOLOGY')
                 kg.parse(ontologySet[loc], format=format)
 
-                fileAnnotations = open(annotationSet[loc], 'r')
-                for annot in fileAnnotations.readlines()[:]:
+                fileAnnotations = open(annotationSet[loc], "r")
+                for annot in tqdm(fileAnnotations.readlines()[:], desc="Loading Annotations", unit="annotation"):
                     annot = annot.lstrip()
 
                     #Get Head and Tail Entities
-                    headInfo, annotations = annot.split(';')
-                    headEnt = f'http://purl.obolibrary.org/obo/{headInfo.split(',')[0]}'
+                    headInfo, annotations, time = annot.split(";")
+                    headEnt = f"http://purl.obolibrary.org/obo/{headInfo.split(',')[0]}"
 
-                    ents.update((ent.strip() for ent in annotations.split(',')))
+                    ents.update((ent.strip() for ent in annotations.split(",")))
 
-                    for urlAnnot in annotations.split(','):
-                        kg.add((URIRef(headEnt), URIRef(f'http://purl.obolibrary.org/obo/{annotationType[loc]}'),
+                    for urlAnnot in annotations.split(","):
+                        kg.add((URIRef(headEnt), URIRef(f"http://purl.obolibrary.org/obo/{annotationType[loc]}"),
                                 URIRef(urlAnnot.strip())))
                 
                 fileAnnotations.close()
-                kg.add(URIRef(f'http://purl.obolibrary.org/obo/{annotationType[loc]}'), RDF.type, OWL.ObjectProperty)
+                kg.add((URIRef(f"http://purl.obolibrary.org/obo/{annotationType[loc]}"), RDF.type, OWL.ObjectProperty))
             
             #Becasuse the ICD9CM ontology is used two parse two different annotation files
             else:
+                print('PARSING THE ONTOLOGY')
                 kg.parse(ontologySet[loc], format=format)
 
                 for step in range(2):
                     trueLoc = loc + step
-                    fileAnnotations = open(annotationSet[trueLoc], 'r')
-                    for annot in fileAnnotations.readlines()[:]:
+                    fileAnnotations = open(annotationSet[trueLoc], "r")
+                    for annot in tqdm(fileAnnotations.readlines()[:], desc="Loading Annotations", unit="annotation"):
                         annot = annot.lstrip()
 
                         #Get Head and Tail Entities
-                        headInfo, annotations = annot.split(';')
-                        headEnt = f'http://purl.obolibrary.org/obo/{headInfo.split(',')[0]}'
+                        headInfo, annotations, time = annot.split(";")
+                        headEnt = f"http://purl.obolibrary.org/obo/{headInfo.split(',')[0]}"
 
-                        ents.update((ent.strip() for ent in annotations.split(',')))
+                        ents.update((ent.strip() for ent in annotations.split(",")))
 
-                        for urlAnnot in annotations.split(','):
-                            kg.add((URIRef(headEnt), URIRef(f'http://purl.obolibrary.org/obo/{annotationType[trueLoc]}'),
+                        for urlAnnot in annotations.split(","):
+                            kg.add((URIRef(headEnt), URIRef(f"http://purl.obolibrary.org/obo/{annotationType[trueLoc]}"),
                                     URIRef(urlAnnot.strip())))
                     
                     fileAnnotations.close()
-                    kg.add(URIRef(f'http://purl.obolibrary.org/obo/{annotationType[trueLoc]}'), RDF.type, OWL.ObjectProperty)
+                    kg.add((URIRef(f"http://purl.obolibrary.org/obo/{annotationType[trueLoc]}"), RDF.type, OWL.ObjectProperty))
 
     except AssertionError as error:
         print(f"Error: {error} Because the number of AnnotationTypes, AnnotationFiles must be the same")
 
-    #kg.serialize(destination='mimicreadmission/rdf2vec/outFiles/myKG.xml')
+    #kg.serialize(destination="/home/ricciard0.dc/mimicReadmission/mimicreadmission/data/myKG.xml")
 
-    print('KG created')
+    print("KG created")
     return kg, list(ents)
-
-
