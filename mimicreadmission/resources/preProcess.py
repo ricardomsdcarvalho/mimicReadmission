34cbce13aba0821dbc77fcb5c26618425d314c18
RmsCarvalho <rmscarvalho@fc.ul.pt>
2024-11-18 16:52:31 +0000

    updated preProcess.py

--- mimicreadmission/resources/preProcess.py
+++ mimicreadmission/resources/preProcess.py
@@ -1,13 +1,14 @@
 import rdflib
 from rdflib import URIRef
 import buildGraph as BG
+import tqdm
 
 def process_kg_for_transe(kg):
     entities = set()
     relations = set()
     triples = []
 
-    for s, p, o in kg:
+    for s, p, o in tqdm(kg, desc="Processing Triples", unit="triple"):
         # Add subject, predicate, and object URIs to entities and relations sets
         entities.add(str(s))
         relations.add(str(p))
@@ -26,27 +27,27 @@ def save_mappings_and_triples(kg, output_path = 'mimicreadmission/data/entityRel
     # Save entity2id.txt
     with open(output_path + 'entity2id.txt', 'w') as f:
         f.write(f"{len(entity2id)}\n")  # Number of entities
-        for entity, idx in entity2id.items():
+        for entity, idx in tqdm(entity2id.items(), desc="Writing Entities", unit="entity"):
             f.write(f"{entity}\t{idx}\n")
 
     # Save relation2id.txt
     with open(output_path + 'relation2id.txt', 'w') as f:
         f.write(f"{len(relation2id)}\n")  # Number of relations
-        for relation, idx in relation2id.items():
+        for relation, idx in tqdm(relation2id.items(), desc="Writing Relations", unit="relation"):
             f.write(f"{relation}\t{idx}\n")
 
     # Save train2id.txt
     with open(output_path + 'train2id.txt', 'w') as f:
         f.write(f"{len(triples)}\n")  # Number of triples
-        for (head, rel, tail) in triples:
+        for (head, rel, tail) in tqdm(triples, desc="Writing Triples", unit="triple"):
             f.write(f"{entity2id[head]}\t{entity2id[tail]}\t{relation2id[rel]}\n")
 
 if __name__ == '__main__':
     # Load the knowledge graph
-    ontologySet = ['mimicreadmission/Data/ontologies/Thesaurus.owl',
-                   'mimicreadmission/Data/ontologies/LOINC.rdf',
-                   'mimicreadmission/Data/ontologies/dron.owl',
-                    'mimicreadmission/Data/ontologies/ICD9CM.ttl'
+    ontologySet = ['mimicreadmission/data/ontologies/Thesaurus.owl',
+                   'mimicreadmission/data/ontologies/LOINC.rdf',
+                   'mimicreadmission/data/ontologies/dron.owl',
+                    'mimicreadmission/data/ontologies/ICD9CM.ttl'
                    ]
     annotationSet = ['mimicreadmission/data/annotations/AnnotationsInitialDiagnosis.csv',
                     'mimicreadmission/data/annotations/AnnotationsLabEvents.csv',
